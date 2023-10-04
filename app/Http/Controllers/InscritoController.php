d745693925c1eee9f09cc5432cba9be82b00f6f6
Iago94 <iagomateusads@hotmail.com>
2023-10-04 18:57:23 -0300

    criação das funcoes do controller de inscritos

---
+++ app/Http/Controllers/InscritoController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Inscrito;
+use Illuminate\Http\Request;
+
+class InscritoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //método index vai retornar todos os inscritos cadastrados
+        $inscritos = Inscrito::paginate(10);
+
+        return response()->json($inscritos);
+    }
+
+    public function show(string $id)
+    {
+        //esse método vai exibir apenas um inscrito quando passado o id
+        try{
+            $inscrito = Inscrito::find($id);
+            
+            $retorno = response()->json([
+                'status' => 1,
+                'id' => $inscrito->id
+            ]);
+
+        }catch(\Exception $e) {
+            $retorno = response()->json([
+                'status' => 0,
+                'message' => $e->getMessage()
+            ]); 
+
+        }
+        return $retorno;
+    }
+    public function store(Request $request)
+    {
+        $regras = [
+            'nome' => 'required',
+            'cpf' => 'required',
+            'email' => 'required'
+        ];
+
+        $mensagens = [
+            'required' => 'O campo :attribute é obrigatório',
+            'email.email' => 'o campo precisa de um email valido'
+        ];
+
+        $this->validate($request, $regras, $mensagens);
+
+        $params = $request->all();
+
+        if(isset($params['cpf']) && !empty($params['cpf'])) {
+            $params['cpf'] = preg_replace( '/[^0-9]/', '', $params['cpf']);
+        }
+
+        try {
+            $model = Inscrito::create($params);
+
+            $retorno = response()->json([
+                'status' => 1,
+                'id' => $model->id
+            ]);
+            
+        } catch(\Exception $e) {
+            $retorno = response()->json([
+                'status' => 0,
+                'message' => $e->getMessage()
+            ]);
+        }
+        
+        return $retorno;
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //atualiza o registro
+        $regras = [
+            'nome' => 'required',
+            'cpf' => 'required',
+            'email' => 'required'
+        ];
+
+        $mensagens = [
+            'required' => 'O campo :attribute é obrigatório',
+            'email.email' => 'o campo precisa de um email valido'
+        ];
+
+        $this->validate($request, $regras, $mensagens);
+
+        $params = $request->all();
+
+        if(isset($params['cpf']) && !empty($params['cpf'])) {
+            $params['cpf'] = preg_replace( '/[^0-9]/', '', $params['cpf']);
+        }
+        
+        try{
+            $inscrito = Inscrito::find($id);
+           
+            $inscrito->update($params);
+            
+            $retorno = response()->json([
+                'status' => 1,
+                'message' => 'Inscrito atualizado com sucesso'
+            ]);
+
+        }catch(\Exception $e) {
+                $retorno = response()->json([
+                    'status' => 0,
+                    'message' => $e->getMessage()
+                ]); 
+        }
+
+        return $retorno;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        
+        try{
+           $inscrito = Inscrito::find($id);
+           
+           $inscrito->delete();
+
+           $retorno = response()->json([
+            'status' => 0,
+            'message' => 'Inscrito excluido com sucesso'
+           ]);
+
+       }catch(\Exception $e) {
+        $retorno = response()->json([
+            'status' => 0,
+            'message' => $e->getMessage()
+        ]); 
+    }
+        return $retorno;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InscritoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(InscritoController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{id}', 'show');
+    Route::post('/', 'store');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
 });
